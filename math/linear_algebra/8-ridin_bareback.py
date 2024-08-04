31a03d2a4703c71650ee37f5349b3fc6a2baf2d4
dArk10R4 <goylerov04@gmail.com>
2024-08-04 23:34:51 +0400

    8

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+that performs matrix multiplication
+"""
+
+def dot(mat1, mat2):
+    """
+    dot
+    """
+    return sum([mat1[i] * mat2[i] for i in range(mat1)])
+
+def mat_mul(mat1, mat2):
+    """
+    matrix multiplication
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+    
+    return [dot(mat1[i], mat2[j]) for j in
+            range(len(mat2[0])) for i in range(len(mat1))]
+
