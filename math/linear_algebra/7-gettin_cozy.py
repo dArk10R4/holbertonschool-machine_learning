ead3e535e6170beade08c2ed7c4b6d1e6f42c806
dArk10R4 <goylerov04@gmail.com>
2024-08-04 23:27:58 +0400

    7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,6 +9,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     concatenates two matrices along a specific axis
     """
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+
         return mat1 + mat2
     
     if len(mat1) != len(mat2):
